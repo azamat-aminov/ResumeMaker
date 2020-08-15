f36188c2322d7d0dcec18f7de577fb921da713a5
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-15 14:27:19 +0500

    EducationDegree and Person models created

---
+++ src/main/java/uz/azamat/demo/model/EducationDegree.java
@@ -0,0 +1,11 @@
+package uz.azamat.demo.model;
+
+import lombok.Data;
+
+import java.sql.Date;
+@Data
+public class EducationDegree {
+    private String universityName;
+    private Date graduatedYear;
+    private String degree;
+}
---
+++ src/main/java/uz/azamat/demo/model/Person.java
@@ -0,0 +1,16 @@
+package uz.azamat.demo.model;
+
+import lombok.Data;
+
+import java.sql.Date;
+import java.util.List;
+
+@Data
+public class Person {
+    private String fullName;
+    private Date dateOfBirth;
+    private String nationality;
+    private List<EducationDegree> educationDegrees;
+    private int phoneNumber;
+    private String email;
+}
