b022873d0a63fc267bbe867b05d3fdd12450e188
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-15 17:33:39 +0500

    controller created

---
+++ src/main/java/uz/azamat/demo/controller/ResumeController.java
@@ -0,0 +1,13 @@
+package uz.azamat.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResumeController {
+
+    @GetMapping
+    public String getPage() {
+        return "person";
+    }
+}
