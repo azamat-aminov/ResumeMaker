0f4a512dfc16535565995735a1ed608ea2ee78dc
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-15 19:03:39 +0500

    login page added

--- src/main/java/uz/azamat/demo/controller/ResumeController.java
+++ src/main/java/uz/azamat/demo/controller/ResumeController.java
@@ -2,12 +2,18 @@ package uz.azamat.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ResumeController {
 
     @GetMapping
     public String getPage() {
+        return "login";
+    }
+
+    @PostMapping("/person")
+    public String getForms(){
         return "person";
     }
 }
