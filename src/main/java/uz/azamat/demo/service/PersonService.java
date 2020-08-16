37fc57edbaa84ed5e8ced6e92af93c20083a3cda
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-16 10:29:47 +0500

    PersonDao assigned as Autowired

--- src/main/java/uz/azamat/demo/service/PersonService.java
+++ src/main/java/uz/azamat/demo/service/PersonService.java
@@ -1,5 +1,6 @@
 package uz.azamat.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uz.azamat.demo.dao.PersonDao;
 import uz.azamat.demo.model.Person;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Service
 public class PersonService {
+    @Autowired
     PersonDao personDao;
 
     public void save(Person person) {
