2a5cafed982bc2cfad7abfa4cca49c147cf0d5d6
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-16 11:26:01 +0500

    Model added to method parameter in the getAllData mapping

--- src/main/java/uz/azamat/demo/controller/ResumeController.java
+++ src/main/java/uz/azamat/demo/controller/ResumeController.java
@@ -1,12 +1,14 @@
 package uz.azamat.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import uz.azamat.demo.model.Person;
 import uz.azamat.demo.service.PersonService;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 public class ResumeController {
@@ -26,7 +28,15 @@ public class ResumeController {
 
     @PostMapping("/savePerson")
     public String savePersons(Person person) {
+        System.out.println(person.getFullName());
         personService.save(person);
+        return "person";
+    }
+
+    @GetMapping("/getAllData")
+    public String getAllData(Model model) {
+        List<Person> allData = personService.getAllData();
+        model.addAttribute("data", allData);
         return "person";
     }
 }
