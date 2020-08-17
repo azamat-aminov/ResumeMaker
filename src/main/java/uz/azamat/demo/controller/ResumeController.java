400c08b5379688861c5d96170930ef1d2689083f
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-17 18:20:10 +0500

    unnecessary import removed

--- src/main/java/uz/azamat/demo/controller/ResumeController.java
+++ src/main/java/uz/azamat/demo/controller/ResumeController.java
@@ -7,7 +7,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import uz.azamat.demo.model.EducationDegree;
 import uz.azamat.demo.model.Person;
 import uz.azamat.demo.service.EducationDegreeService;
 import uz.azamat.demo.service.PersonService;
