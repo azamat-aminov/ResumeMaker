454320d1546fa211ad90b3547670df39fd79f020
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-18 17:04:03 +0500

    get information about University where person studied

--- src/main/java/uz/azamat/demo/controller/ResumeController.java
+++ src/main/java/uz/azamat/demo/controller/ResumeController.java
@@ -7,13 +7,16 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import uz.azamat.demo.model.EducationDegree;
 import uz.azamat.demo.model.Person;
 import uz.azamat.demo.service.EducationDegreeService;
 import uz.azamat.demo.service.PersonService;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.List;
+import java.sql.Date;
+import java.util.*;
 
 @Controller
 public class ResumeController {
@@ -27,21 +30,41 @@ public class ResumeController {
     public String getLoginPage() {
         return "login";
     }
+
     @GetMapping("/createPerson")
-    public String createPerson(Person person) {
+    public String createPerson(Person person, EducationDegree edu) {
         return "person";
     }
 
     @PostMapping("/form")
-    public String callFormPage(Person person) {
+    public String callFormPage(Person person, EducationDegree edu) {
         return "person";
     }
 
     @PostMapping("/savePerson")
-    public String savePersons(Person person, Model model, BindingResult result) {
+    public String savePersons(Person person, Model model, BindingResult result,
+                              EducationDegree edu, HttpServletRequest req) {
         if (result.hasErrors()) {
             return "person";
         }
+        List<EducationDegree> educationDegrees = new ArrayList<>();
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        String[] universityNames = parameterMap.get("universityName");
+        String[] gradYear = parameterMap.get("graduatedYear");
+        String[] degrees = parameterMap.get("degree");
+        for (int i = 0; i < universityNames.length; i++) {
+            String universityName = universityNames[i];
+            String graduatedYear = gradYear[i];
+            String degree = degrees[i];
+            Date date = Date.valueOf(graduatedYear);
+            edu.setUniversityName(universityName);
+            edu.setGraduatedYear(date);
+            edu.setDegree(degree);
+            educationDegrees.add(edu);
+        }
+
+        educationDegreeService.save(edu);
         personService.save(person);
         List<Person> allData = personService.getAllData();
         model.addAttribute("data", allData);
@@ -73,9 +96,10 @@ public class ResumeController {
         model.addAttribute("data", personService.getAllData());
         return "resumes";
     }
+
     @GetMapping("/data/by/{id}")
-    public String getOnePerson(@PathVariable int id, Model model){
-         model.addAttribute("onePerson", personService.findById(id));
+    public String getOnePerson(@PathVariable int id, Model model) {
+        model.addAttribute("onePerson", personService.findById(id));
         return "onePersonInfo";
     }
 
