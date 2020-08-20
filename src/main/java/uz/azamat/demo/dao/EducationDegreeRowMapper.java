71f1f6ab4e688c157c365d5571099ebf823a874d
Azamat <azamatmaqsudjonovich@gmail.com>
2020-08-20 21:10:36 +0500

    id field removed and universityId to EducationDegree

--- src/main/java/uz/azamat/demo/dao/EducationDegreeRowMapper.java
+++ src/main/java/uz/azamat/demo/dao/EducationDegreeRowMapper.java
@@ -10,11 +10,10 @@ public class EducationDegreeRowMapper implements RowMapper<EducationDegree> {
     @Override
     public EducationDegree mapRow(ResultSet resultSet, int i) throws SQLException {
         EducationDegree degree = new EducationDegree();
+        degree.setUniversityId(resultSet.getInt("univer_id"));
         degree.setUniversityName(resultSet.getString("name"));
         degree.setGraduatedYear(resultSet.getDate("graduated_year"));
         degree.setDegree(resultSet.getString("degree"));
-        degree.setId(resultSet.getInt("id"));
-
         return degree;
     }
 }
--- src/main/java/uz/azamat/demo/model/EducationDegree.java
+++ src/main/java/uz/azamat/demo/model/EducationDegree.java
@@ -3,10 +3,11 @@ package uz.azamat.demo.model;
 import lombok.Data;
 
 import java.sql.Date;
+
 @Data
 public class EducationDegree {
+    private int universityId;
     private String universityName;
     private Date graduatedYear;
     private String degree;
-    private int id;
 }
